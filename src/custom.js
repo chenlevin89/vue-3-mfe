abf77d6fc2b505a797d3271a5185702836d7fc06
chen.levin <chen.levin@ironsrc.com>
2022-01-27 13:33:30 +0200

    stable

---
+++ src/custom.js
@@ -0,0 +1,3 @@
+export default function Custom(){
+    console.log('test vue mfe')
+}
---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const dependencies = require('./package.json').dependencies;
+
+module.exports = {
+    publicPath: process.env.PUBLIC_PATH,
+    configureWebpack: {
+        plugins: [
+            new ModuleFederationPlugin({
+                name: "vueRemote",
+                library: {type: "var", name: "vueRemote"},
+                filename: "remoteEntry.js",
+                exposes: {
+                    './Test': {
+                        import: './src/custom.js',
+                        name:`src_vue_custom`
+                    }
+                },
+                shared: {
+                    'vue': {requiredVersion: `${dependencies['vue']}`, singleton: true},
+                    'core-js': {requiredVersion: `${dependencies['core-js']}`, singleton: true},
+                    'vue-router': {requiredVersion: `${dependencies['vue-router']}`, singleton: true}
+                }
+            })
+        ],
+        devServer: {
+            historyApiFallback: true,
+            port:8082
+        }
+    }
+}
